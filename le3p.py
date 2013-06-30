7f17d2c435b093b4521cc3d46f281bf7f173a6fb
Scott Maher <scott.a.maher@gmail.com>
2013-06-30 16:49:03 -0400

    Update le3p.py
    added licence header

--- le3p.py
+++ le3p.py
@@ -1,8 +1,23 @@
 #le3p v0.1.3
 
-# LE3P is a "Large Envelope 3d Printer"
-# a Rep-Rap type machine built with square T-slot extruded beams 
-# currently working with OpenBeam 15mm beam (www.openbeamusa.com)
+# Le3p - A larger envelope, t-slot extrusion based, H-Gantry 3D printer.
+#
+# Copyright (c) 2013 Scott Maher scott.a.maher@gmail.com
+#
+# This file is part of Le3p
+
+# Le3p is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Le3p is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Le3p.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import division
 import sys
